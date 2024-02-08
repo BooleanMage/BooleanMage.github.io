41df7945c25046e371511eea11195e683758863b
BooleanMage <BooleanMage@users.noreply.github.com>
2024-02-08 16:08:30 +0800

    upload

---
+++ _includes_/README.md
@@ -0,0 +1,9 @@
+# jekyll_includes
+
+
+## 
+```
+$ gem install bundler
+$ bundle install
+$ bundle exec jekyll serve --port 4001
+```
--- upload.bat
+++ upload.bat
@@ -1,17 +1,6 @@
-cd _includes
-./upload.bat
-cd ../
-
-cd _layouts
-./upload.bat
-cd ../
-
-
 git config --local user.name "BooleanMage"
 git config --local user.email "BooleanMage@users.noreply.github.com"
 git remote set-url origin git@BM:BooleanMage/BooleanMage.github.io.git
-
-git submodule update --recursive --remote
 
 git pull
 git add .
